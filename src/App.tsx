3dd8e52d0709ee7207716334b207fb1561fa173f
Okereke Clement Kalu <138951962+inspikalu@users.noreply.github.com>
2024-08-11 11:55:26 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -67,7 +67,7 @@ const App: React.FC = () => {
 
   const randomize = () => {
     const randomHat = Math.floor(Math.random() * 21) + 1;
-    const randomAccessory = Math.floor(Math.random() * 12) + 1;
+    const randomAccessory = Math.floor(Math.random() * 11) + 1;
     const randomHand = Math.floor(Math.random() * 20) + 1;
     const randomHead = Math.floor(Math.random() * 16) + 1;
     const randomOutfit = Math.floor(Math.random() * 21) + 1;
